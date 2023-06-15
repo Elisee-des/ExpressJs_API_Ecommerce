04cec9137cd4e77b75511c512f34101df16100f3
Elisee-des <esabidani@gmail.com>
2023-06-15 13:48:34 +0000

    Creation d'un utilisateur

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,6 +1,22 @@
 const User = require("../models/userModel")
 
 
-const createUser = async (request, response) => {
-    
-}
+const createUser = async (req, res) => {
+    const email = req.body.email;
+    const findUser = await User.findOne({email: email})
+    if(!findUser)
+    {
+        //Creation d'un utilisateur
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    }
+    else {
+        //L'utilisateur existe deja
+        res.json({
+            msg: "L'utilisateur existe deja",
+            success: false
+        });
+    }
+};
+
+module.exports = {createUser}
