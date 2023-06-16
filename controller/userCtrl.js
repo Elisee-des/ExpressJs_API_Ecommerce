05555edb7310d20c5a00773e91d41f32cbce4cf7
Elisee-des <esabidani@gmail.com>
2023-06-16 00:56:59 +0000

    Detail d'un utiisateur

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -2,7 +2,7 @@ const { generateToken } = require("../config/jsonToken");
 const User = require("../models/userModel")
 const asyncHandler = require('express-async-handler')
 
-
+//Creation d'un user
 const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({email: email})
@@ -17,6 +17,7 @@ const createUser = asyncHandler(async (req, res) => {
     }
 })
 
+//Connexion d'un user
 const loginUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body;
     //on verifie si l'utilisateur exite
@@ -37,14 +38,28 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 });
 
+//Recuperation de tous les utilisateurs
 const getAllUsers = asyncHandler(async(req, res) => {
     try
     {
-        const getUsers = await User.find()
-        res.json(getUsers)
+        const getUsers = await User.find();
+        res.json(getUsers);
     }catch(error){
+        throw new Error(error);
+    }
+})
+
+//Recuperation d'un user
+const getUser = asyncHandler(async(req, res) => {
+    const {id} = req.params;
+    try
+    {
+        const getUser = await User.findById(id);
+        res.json(getUser)
+    }catch(error)
+    {
         throw new Error(error)
     }
 })
 
-module.exports = {createUser, loginUser, getAllUsers}
+module.exports = {createUser, loginUser, getAllUsers, getUser}
